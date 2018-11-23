2ea0017605a133028ece8547e81799218427abde
Roee FL <Roeefl@gmail.com>
2018-11-23 19:25:55 +0200

    Updated auth.js
    Changed facebook callback

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '285457868749583', // your App ID
         'clientSecret'  : '065c125e5b4c3ec0eaeae76fa2ed3e3a', // your App Secret
-        'callbackURL'   : 'http://localhost:9000/login/facebook/return',
+        'callbackURL'   : 'http://localhost:9000/login/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     },
