17886e8ba80f81b8ce04a74db065338d32710649
Roee FL <Roeefl@gmail.com>
2019-01-20 20:34:53 +0200

    heroku-fix-5

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
 //     res.end(req.user || false);
 //     });
 
-    app.get('/login/google/callback      ',
+    app.get('/login/google/callback',
         passport.authenticate('google', { failureRedirect: '/' }),
         (req, res) => {
             res.redirect(process.env.REDIRECT_DOMAIN);
